11be84d675e0c41fcef6c1f109037e009156e36e
Akram Ouardas <a.ouardas@esi-sba.dz>
2021-02-15 18:31:49 +0100

    adding full page scroll to service V3

--- src/Containers/Services/Services.jsx
+++ src/Containers/Services/Services.jsx
@@ -5,69 +5,93 @@ import classes from "./Services.module.css";
 import Image from "../../assets/Home_Photo.png";
 import Offres from "../../Components/Offres/Offres";
 import Carousel from "../../Components/UI/Carousel/Carousel";
+
+import ScrollableContainer from "react-full-page-scroll";
+
+const PageComponent = (props) => {
+  return props.children;
+};
+
 class Services extends Component {
   state = {
     offres: [
-      { id : 1,
+      {
+        id: 1,
         titre: "offre 1",
         description: "c'est le premier offre pour les services",
         image: Image,
       },
-      { id : 2,
+      {
+        id: 2,
 
         titre: "offre 2",
         description: "c'est le dexieme offre pour les services",
         image: Image,
       },
-      { id : 3,
+      {
+        id: 3,
         titre: "offre 3",
         description: "c'est le 3eme offre pour les services",
         image: Image,
       },
-      { id : 4,
+      {
+        id: 4,
         titre: "offre 3",
         description: "c'est le 3eme offre pour les services",
         image: Image,
       },
-      { id : 5,
+      {
+        id: 5,
         titre: "offre 3",
         description: "c'est le 3eme offre pour les services",
         image: Image,
       },
-      { id : 6,
+      {
+        id: 6,
         titre: "offre 3",
         description: "c'est le 3eme offre pour les services",
         image: Image,
       },
     ],
-    carousel_images: [{id : 1, image: Image }, {id : 2, image: Image }, {id : 3, image: Image }],
+    carousel_images: [
+      { id: 1, image: Image },
+      { id: 2, image: Image },
+      { id: 3, image: Image },
+    ],
   };
   render() {
     const service = "Service irrigation";
     return (
       <Aux>
         <div className={classes.Container}>
-          <h1>Service irrigation</h1>
-          <div className={classes.Top_Div}>
-            <div className={classes.Top_Left_Div}>
-              <Carousel images ={this.state.carousel_images} />
-            </div>
+          <ScrollableContainer animationTime={500}>
+            <PageComponent>
+              <h1>Service irrigation</h1>
+              <div className={classes.Top_Div}>
+                <div className={classes.Top_Left_Div}>
+                  <Carousel images={this.state.carousel_images} />
+                </div>
 
-            <div className={classes.Top_Right_Div}>
-              <p>
-                <strong> {service.toLocaleUpperCase()} </strong> c'est la
-                description du systeme on va le faire kan on termine le design
-                car c'est qlq chose de details w wahdine rahoum mreyhine c'est
-                la description du systeme on va le faire kan on termine le
-                design car c'est qlq chose de details w wahdine rahoum mreyhine
-              </p>
-            </div>
-          </div>
+                <div className={classes.Top_Right_Div}>
+                  <p>
+                    <strong> {service.toLocaleUpperCase()} </strong> c'est la
+                    description du systeme on va le faire kan on termine le
+                    design car c'est qlq chose de details w wahdine rahoum
+                    mreyhine c'est la description du systeme on va le faire kan
+                    on termine le design car c'est qlq chose de details w
+                    wahdine rahoum mreyhine
+                  </p>
+                </div>
+              </div>
+            </PageComponent>
 
-          <h1>Choisir une offre</h1>
-          <div className={classes.Bottom_Div}>
-            <Offres offres={this.state.offres} />
-          </div>
+            <PageComponent page={2}>
+              <div className={classes.Bottom_Div}>
+                <h1>Choisir une offre</h1>
+                <Offres offres={this.state.offres} />
+              </div>
+            </PageComponent>
+          </ScrollableContainer>
         </div>
       </Aux>
     );
--- src/Hoc/Layout/Layout.jsx
+++ src/Hoc/Layout/Layout.jsx
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import classes from "./Layout.module.css";
 import Aux from "../Auxilary/Auxilary";
 import Toolbar from "../../Components/Navigation/Toolbar/Toolbar";
+
+
 class Layout extends Component {
   render(props) {
     return (
